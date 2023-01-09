f604e6694d17e20a5bc377626b8d5c6faa975491
Your Name~clear <you@example.com>
2023-01-09 16:46:00 +0100

    Anim on load OK + form page creation + more about site page

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,21 @@
-let date = new Date().getFullYear();
-document.getElementById("date").innerHTML = date;
-
+$(document).ready(function() {
+//get the actual year
+    let date = new Date().getFullYear();
+    document.getElementById("date").innerHTML = date;
+//Stop load anim when the anim is finished    
+    setTimeout(function() {
+      $('.project').removeClass('tilt-in-fwd-br');
+      $('.project').removeClass('tilt-in-fwd-tl');
+    },1300);
+//Add the :hover anim when the load anim is finished (pb is that :hover reset other anim)
+    setTimeout(function() {
+        $('.project').addClass('scale-down-center');
+    }, 1310);
+//Form focus anim
+$('input , textarea').focusin(function(){
+    $(this).addClass('formfocus')
+});
+$('input , textarea').focusout(function(){
+    $(this).removeClass('formfocus')
+});
+  });
